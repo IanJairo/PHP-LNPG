892b2ddf6b6213a6b6c5bb753affdf1f2591e7d2
Haul Muller <112398857+HaulMuller@users.noreply.github.com>
2023-05-08 11:47:34 -0300

    Update switch.php

--- src/estrutura_condição/switch.php
+++ src/estrutura_condição/switch.php
@@ -2,18 +2,18 @@
 
 $corCasa = "vermelho"; //declaração de variável a ser comparada com o switch
 
-switch ($corCasa):
+switch ($corCasa): //colocar entre parênteses a variável a ser comparada com as condições que serão estabelecidas.
     case "vermelho":
-      echo "Sua casa é vermelha"; // código a ser executado se $corCasa for igual a "vermelho"
+      echo "Sua casa é vermelha"; // código a ser executado se $corCasa for igual a "vermelho".
       break;
     case "amarelo":
-      echo "Sua casa é amarela"; // código a ser executado se $corCasa for igual a "amarelo"
+      echo "Sua casa é amarela"; // código a ser executado se $corCasa for igual a "amarelo".
       break;
     case "azul":
-      echo "Sua casa é azul"; // código a ser executado se $corCasa for igual a "azul"
+      echo "Sua casa é azul"; // código a ser executado se $corCasa for igual a "azul".
       break;
-    // e assim por diante
+    // e assim por diante,
     default:
-      echo "Sua casa não é vermelha, azul ou amarela."; // código a ser executado se $expressao não corresponder a nenhum valor
+      echo "Sua casa não é vermelha, azul ou amarela."; // código a ser executado se $expressao não corresponder a nenhum dos valores,
 
 endswitch; //finaliza o bloco de código do switch, podendo prosseguir com o código.
